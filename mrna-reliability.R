85edbc9374c4861e7c9338e6333cbc6e7c28e93b
Alexander Franks <afranks@fas.harvard.edu>
2016-03-02 13:42:13 -0800

    Compute reliability of mRNA data

--- mrna-reliability.R
+++ mrna-reliability.R
@@ -2,39 +2,38 @@ library(plyr)
 library(mvtnorm)
 source("util_functions.R")
 
-mcp <- read.csv("~/Dropbox/NatureComment/ENCODE_mRNA_Data/mcp-mrna.csv", row.names=1)
-mcp[mcp==0] <- NA
+mrna_pa <- read.csv("data/mrna_pa.csv", row.names=1)
+mrna_illumina <- read.csv("data/mrna_illumina.csv", row.names=1)
+mrna_mcp <- read.csv("data/mrna_mcp.csv", row.names=1)
 
-ilumina <- read.csv("~/course/tissue-ptr/mrna_ilumina.csv", row.names=1)
+cols <- intersect(colnames(mrna_pa), colnames(mrna_illumina)) %>% 
+    intersect(colnames(mrna_mcp))
 
-encode <- read.csv("~/Dropbox/NatureComment/ENCODE_mrna_Data/57epigenomes.exon.RPKM.pc.csv")
-encodeLegend <- read.table("~/Dropbox/NatureComment/ENCODE_mrna_Data/57epigenomes.exon.RPKM.pc_EG.name.txt", row.names=1, stringsAsFactors=FALSE)
+rows <- intersect(rownames(mrna_pa), rownames(mrna_illumina)) %>% 
+    intersect(rownames(mrna_mcp))
 
-encodeColnames <-  tolower(sapply(colnames(encode)[-(1:2)],
-                          function(nm) encodeLegend[nm, ]))
-indices <- sapply(c("adult_liver", "gastric"),
-                  function(x) grep(x, encodeColnames))
-encodeColnames[indices] <- c("liver", "stomach")
+###################
+## MCP vs Illumina
+###################
 
-colnames(encode)[-(1:2)] <- encodeColnames
-encode[encode==0] <- NA
+out <- getZscores(as.matrix(mrna_mcp[rows, cols]),
+                  as.matrix(mrna_illumina[rows, cols]))
+within.cors <- out$within.cors
 
-## average within gene 
-avgEncode <- ddply(as.data.frame(encode), "gene_id", function(x) {
-    colMeans(x[, -(1:2)], na.rm=TRUE)
-})
-rownames(avgEncode) <- avgEncode[, 1]
-avgEncode <- avgEncode[, -1]
+###################
+## MCP vs PA
+###################
 
-write.csv(avgEncode, "~/Dropbox/NatureComment/ENCODE_mrna_Data/encode_processed.csv")
+out <- getZscores(as.matrix(mrna_mcp[rows, cols]),
+                  as.matrix(mrna_pa[rows, cols]))
+within.cors <- out$within.cors
 
-cols <- intersect(colnames(mcp), colnames(avgEncode))
-## rows <- intersect(rownames(mcp), rownames(avgEncode))
-cols <- intersect(colnames(mcp), colnames(ilumina))
-rows <- intersect(rownames(mcp), rownames(ilumina))
+###################
+## Illumina vs PA
+###################
 
-## out <- getZscores(as.matrix(mcp[rows, cols]), as.matrix(avgEncode[rows, cols]))
-out <- getZscores(as.matrix(mcp[rows, cols]), as.matrix(ilumina[rows, cols]))
+out <- getZscores(as.matrix(mrna_illumina[rows, cols]),
+                  as.matrix(mrna_pa[rows, cols]))
 within.cors <- out$within.cors
 
 sim.within.cors <- sapply(1:length(out$z.score), function(i) {
@@ -43,13 +42,17 @@ sim.within.cors <- sapply(1:length(out$z.score), function(i) {
                 sigma=matrix(c(1, out$pop.cor, out$pop.cor, 1), nrow=2)))[1,2] 
 })
 
+###################
+## Histogram of correlations for Illumina/PA reliablity
+###################
+
 pdf("Figs/mrna_sim_and_within_cors.pdf")
 pooled.cors <- c(sim.within.cors, within.cors)
 label <- factor(rep(c("Simulated", "Empirical"), 
                     levels=c("Simulated", "Empirical"), 
                     each=length(within.cors)), ordered=TRUE)
 ggplot(data.frame(cors=pooled.cors, cut=label), aes(x=cors, fill=label)) + 
-geom_bar(position=position_dodge(width=0.05), binwidth=0.1, size=0,
+geom_histogram(position=position_dodge(width=0.05), binwidth=0.08, size=0,
          aes(order=c(rep(c(1,2), each=length(within.cors))))) +
     labs(x=expression(paste("Correlation, ", R[P])), 
          y="", title=expression("Across Tissue")) +
@@ -65,6 +68,3 @@ geom_bar(position=position_dodge(width=0.05), binwidth=0.1, size=0,
     scale_x_continuous(limits=c(-1, 1)) +
     geom_vline(xintercept = out$pop.cor, linetype=2)
 dev.off()
-
-
-cor(avgEncode[, c("aorta", "left_ventricle", "right_ventricle", "right_atrium")], use="pairwise.complete.obs")
